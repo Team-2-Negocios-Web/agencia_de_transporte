db27d5adb18a2aeeaff85e093a923d849f57e9f8
gustavocano23 <gustavo00inmare@gmail.com>
2021-07-27 03:21:42 -0600

    agregando la programacion de buses y confirmacion del ticket

--- app_transportAgency/urls.py
+++ app_transportAgency/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.travels, name="travels_view" ),
     path('ticket/', views.route, name="ticket_view" ),
     path('ticket/ajax', views.ticket, name="ajax_view" ),
+    path('buses/', views.list_buses, name="list_buses")
 ]
