289bd4727da7aa3f314dd2654d479fbe8593d031
Carlos Cuadra <cuadracarlos693@gmail.com>
2021-07-30 00:30:37 -0600

    Agregando diseño a la tabla-modal

--- app_transportAgency/views.py
+++ app_transportAgency/views.py
@@ -262,7 +262,7 @@ def list_buses(request):
                                 <td>{p.client}</td>
                                 <td>{p.companion}</td>
                                 <td>{p.seating}</td>
-                                <td><button type="button" class="btn btn-primary" data-bs-dismiss="modal" >Ver cliente</button></td>
+                                <td><a href="#" style="outline=none; text-decoration=none;"><i class="fa fa-user-circle" aria-hidden="true" style="color=red"></i></a></td>
                             <tr>
                         '''
             #else:
